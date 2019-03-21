867b3a6860e218e520ff2d22092dc5997965839a
Edwin Rozendom <edwin.rozendom@bcc.no>
2019-03-21 12:30:11 +0000

    Merged PR 1340: First version of oidc-costumizations plugin
    First version of plugin
    
    Related work items: #4368

---
+++ Plugins/openid-connect-bcc-customizations/openid-connect-bcc-customizations.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name: OpenID Connect Generic (BCC signon customization)
+Description: customazations for the BCC signon system. (support: it@bcc.no)
+Version: 1.0.0
+Author: BCC
+*/
+
+$bcc_auth_domain = "https://auth.bcc.no/";
+
+/**
+ * Allow logout without confirmation
+ */
+add_action('check_admin_referer', 'logout_without_confirm', 10, 2);
+function logout_without_confirm($action, $result)
+{
+    if ($action == "log-out" && !isset($_GET['_wpnonce']) && isset($_GET['signoutiframe']) ) {
+        $redirect_to = isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : '';
+        $location = str_replace('&amp;', '&', wp_logout_url($redirect_to));;
+        header("Location: $location");
+        die;
+    }
+}
+
+/* Add script tag for single signout */
+add_action('wp_head', 'add_signout_script');
+function add_signout_script(){
+	global $bcc_auth_domain;
+
+	echo ('<script type="text/javascript" src="' . $bcc_auth_domain . 'signout/js" signout-path="/wp-login.php?action=logout&signoutiframe=1"></script>');
+};
+
+/**
+ * Handle openid connect errors better by redirecting to the 'bccAuthDomain'
+ */
+add_action( 'wp_authenticate', 'handle_openid_error');
+function handle_openid_error(){
+  	global $bcc_auth_domain;
+
+  	$error = $_GET["login-error"];
+  	switch ($error){
+		case "unknown-error":
+	    	wp_redirect($bcc_auth_domain . '?message=consentrejected');
+	    	exit;    
+	    break;
+		case "missing-state":
+	  		wp_redirect(home_url());
+	  		exit;    
+	  	break;
+		default:
+	  	break;
+  	}
+}
