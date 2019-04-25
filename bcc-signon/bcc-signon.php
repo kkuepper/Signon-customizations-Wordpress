3dc940f96b3bc0da0fbb0f215b29e9fbaf0b80a1
Edward Tombre <edward.tombre@bcc.no>
2019-04-25 14:37:55 +0200

    Structure plugin to separate OpenId Connect from our customization

---
+++ bcc-signon/bcc-signon.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: BCC Signon
+Description: BCC signon system (support: it@bcc.no).
+Version: 1.0.0
+Author: BCC
+*/
+
+require plugin_dir_path( dirname( __FILE__) ) . 'openid-connect-bcc-customizations/bcc-signon.php';
+require plugin_dir_path( dirname( __FILE__) ) . 'openid-connect-generic-client/openid-connect-generic.php';
+?>
