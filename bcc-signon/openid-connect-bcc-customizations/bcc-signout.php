f9e8252481c2f809d3f7fe5ed9601f50a6f799fa
Edwin Rozendom <edwin.rozendom@bcc.no>
2019-05-03 08:58:59 +0000

    Merged PR 1552: Implemented singout of widgets into the plugin
    Implemented singout of widgets into the plugin
    
    Related work items: #11741

---
+++ bcc-signon/openid-connect-bcc-customizations/bcc-signout.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>signout</title>
+</head>
+<body>
+<div>
+    <script src="https://widgets.bcc.no/widgets/signoutjs"></script>
+    <script>
+    function getQueryString(input){
+    if (input == "") return {};
+    var result = {};
+    for (var i = 0; i < input.length; ++i)
+    {
+        var p=input[i].split('=', 2);
+        if (p.length == 1)
+            result[p[0]] = "";
+        else
+          result[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return result;
+    };
+    
+    var qs = getQueryString(window.location.search.substr(1).split('&'))
+    window.location = qs["redirectpath"];   
+    </script>
+</div>
+</body>
+</html>
