60cc87d0e4ffd2938139c2fd17837e57add6d56c
Edward Tombre <edward.tombre@bcc.no>
2019-06-14 10:57:48 +0000

    Merged PR 1755: Add settings links to the plugin page
    Add settings links to the plugin page

--- bcc-signon/bcc-signon.php
+++ bcc-signon/bcc-signon.php
@@ -10,6 +10,22 @@ Author: BCC IT
 require  dirname( __FILE__) . '/openid-connect-bcc-customizations/bcc-signon.php';
 require  dirname( __FILE__) . '/openid-connect-generic-client/openid-connect-generic.php';
 require dirname( __FILE__)  . '/plugin-update-checker/plugin-update-checker.php';
+
+function plugin_settings_link($links) {
+	$oidc_url = get_admin_url() . 'options-general.php?page=openid-connect-generic-settings';
+	$oidc_settings_link = '<a href="'.$oidc_url.'">' . 'OIDC Settings' . '</a>';
+	$bcc_url = get_admin_url() . 'options-general.php?page=bcc_signon_settings_page';
+	$bcc_settings_link = '<a href="'.$bcc_url.'">' . 'BCC Settings' . '</a>';
+	array_unshift( $links, $oidc_settings_link );
+	array_unshift( $links, $bcc_settings_link );
+	return $links;
+}
+ 
+function plugin_after_setup_theme() {
+     add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'plugin_settings_link');
+}
+add_action ('after_setup_theme', 'plugin_after_setup_theme');
+
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
     'https://developer.bcc.no/api/updates/bcc-signon.json',
      __FILE__,
