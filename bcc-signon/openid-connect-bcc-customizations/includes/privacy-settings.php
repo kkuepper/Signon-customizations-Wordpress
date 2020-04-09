ada511d56809f272be816e9d1da3a94c916bf93d
Edward Tombre <edward.tombre@bcc.no>
2020-04-09 11:33:22 +0000

    Merged PR 3250: Disable rest api for unauthenticated users
    Disable rest api for unauthenticated users

--- bcc-signon/openid-connect-bcc-customizations/includes/privacy-settings.php
+++ bcc-signon/openid-connect-bcc-customizations/includes/privacy-settings.php
@@ -26,5 +26,30 @@ function bcc_privacy_settings() {
     }
 }
 add_action( 'template_redirect', 'bcc_privacy_settings' );
- 
+
+/* DISABLE REST API FOR NON-LOGGED IN USERS */
+remove_action('template_redirect', 'rest_output_link_header', 11);
+remove_action('wp_head', 'rest_output_link_wp_head', 10);
+remove_action('xmlrpc_rsd_apis', 'rest_output_rsd');
+
+if (version_compare(get_bloginfo('version'), '4.7', '>=')) {
+	add_filter('rest_authentication_errors', 'disable_wp_rest_api');
+} else {
+	disable_wp_rest_api_legacy();
+}
+
+function disable_wp_rest_api($access) {
+	if (!is_user_logged_in()) {
+		$message = apply_filters('disable_wp_rest_api_error', __('REST API restricted to authenticated users.', 'disable-wp-rest-api'));
+		return new WP_Error('rest_login_required', $message, array('status' => rest_authorization_required_code()));
+	}
+	return $access;
+}
+
+function disable_wp_rest_api_legacy() {
+    add_filter('json_enabled', '__return_false');
+    add_filter('json_jsonp_enabled', '__return_false');
+    add_filter('rest_enabled', '__return_false');
+    add_filter('rest_jsonp_enabled', '__return_false');
+}
 ?>
